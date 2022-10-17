f62019edfd56970329ee01fb601377bd63b8d3a2
Rémi Courtel <14078835+recrtl@users.noreply.github.com>
2022-10-17 20:33:22 +0200

    feat: update evolution chain struct

--- structs/evolution.go
+++ structs/evolution.go
@@ -1,78 +1,44 @@
 package structs
 
+type EvolutionDetail struct {
+	Gender                int         `json:"gender"`
+	HeldItem              interface{} `json:"held_item"`
+	Item                  interface{} `json:"item"`
+	KnownMove             interface{} `json:"known_move"`
+	KnownMoveType         interface{} `json:"known_move_type"`
+	Location              interface{} `json:"location"`
+	MinAffection          int         `json:"min_affection"`
+	MinBeauty             int         `json:"min_beauty"`
+	MinHappiness          int         `json:"min_happiness"`
+	MinLevel              int         `json:"min_level"`
+	NeedsOverworldRain    bool        `json:"needs_overworld_rain"`
+	PartySpecies          interface{} `json:"party_species"`
+	PartyType             interface{} `json:"party_type"`
+	RelativePhysicalStats int         `json:"relative_physical_stats"`
+	TimeOfDay             string      `json:"time_of_day"`
+	TradeSpecies          interface{} `json:"trade_species"`
+	Trigger               struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"trigger"`
+	TurnUpsideDown bool `json:"turn_upside_down"`
+}
+
+type EvolutionChainLink struct {
+	EvolutionDetails []EvolutionDetail    `json:"evolution_details"`
+	EvolvesTo        []EvolutionChainLink `json:"evolves_to"`
+	IsBaby           bool                 `json:"is_baby"`
+	Species          struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"species"`
+}
+
 // EvolutionChain is a single evolution chain.
 type EvolutionChain struct {
-	BabyTriggerItem interface{} `json:"baby_trigger_item"`
-	Chain           struct {
-		EvolutionDetails []interface{} `json:"evolution_details"`
-		EvolvesTo        []struct {
-			EvolutionDetails []struct {
-				Gender                interface{} `json:"gender"`
-				HeldItem              interface{} `json:"held_item"`
-				Item                  interface{} `json:"item"`
-				KnownMove             interface{} `json:"known_move"`
-				KnownMoveType         interface{} `json:"known_move_type"`
-				Location              interface{} `json:"location"`
-				MinAffection          interface{} `json:"min_affection"`
-				MinBeauty             interface{} `json:"min_beauty"`
-				MinHappiness          interface{} `json:"min_happiness"`
-				MinLevel              int         `json:"min_level"`
-				NeedsOverworldRain    bool        `json:"needs_overworld_rain"`
-				PartySpecies          interface{} `json:"party_species"`
-				PartyType             interface{} `json:"party_type"`
-				RelativePhysicalStats interface{} `json:"relative_physical_stats"`
-				TimeOfDay             string      `json:"time_of_day"`
-				TradeSpecies          interface{} `json:"trade_species"`
-				Trigger               struct {
-					Name string `json:"name"`
-					URL  string `json:"url"`
-				} `json:"trigger"`
-				TurnUpsideDown bool `json:"turn_upside_down"`
-			} `json:"evolution_details"`
-			EvolvesTo []struct {
-				EvolutionDetails []struct {
-					Gender                interface{} `json:"gender"`
-					HeldItem              interface{} `json:"held_item"`
-					Item                  interface{} `json:"item"`
-					KnownMove             interface{} `json:"known_move"`
-					KnownMoveType         interface{} `json:"known_move_type"`
-					Location              interface{} `json:"location"`
-					MinAffection          interface{} `json:"min_affection"`
-					MinBeauty             interface{} `json:"min_beauty"`
-					MinHappiness          interface{} `json:"min_happiness"`
-					MinLevel              int         `json:"min_level"`
-					NeedsOverworldRain    bool        `json:"needs_overworld_rain"`
-					PartySpecies          interface{} `json:"party_species"`
-					PartyType             interface{} `json:"party_type"`
-					RelativePhysicalStats interface{} `json:"relative_physical_stats"`
-					TimeOfDay             string      `json:"time_of_day"`
-					TradeSpecies          interface{} `json:"trade_species"`
-					Trigger               struct {
-						Name string `json:"name"`
-						URL  string `json:"url"`
-					} `json:"trigger"`
-					TurnUpsideDown bool `json:"turn_upside_down"`
-				} `json:"evolution_details"`
-				EvolvesTo []interface{} `json:"evolves_to"`
-				IsBaby    bool          `json:"is_baby"`
-				Species   struct {
-					Name string `json:"name"`
-					URL  string `json:"url"`
-				} `json:"species"`
-			} `json:"evolves_to"`
-			IsBaby  bool `json:"is_baby"`
-			Species struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"species"`
-		} `json:"evolves_to"`
-		IsBaby  bool `json:"is_baby"`
-		Species struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"species"`
-	} `json:"chain"`
-	ID int `json:"id"`
+	BabyTriggerItem interface{}        `json:"baby_trigger_item"`
+	Chain           EvolutionChainLink `json:"chain"`
+	ID              int                `json:"id"`
 }
 
 // EvolutionTrigger is a single evolution trigger.
